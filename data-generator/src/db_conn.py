3ddfb6ed2a65527d5e12e07ddb326808807cb2d6
VL1507 <vova.mashyanov@gmail.com>
2026-01-19 02:37:59 +1000

    create db session

---
+++ data-generator/src/db_conn.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from config import DB_URL
+
+engine = create_engine(DB_URL, echo=False)
+
+
+Session = sessionmaker(bind=engine)
