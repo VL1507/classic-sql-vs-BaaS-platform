0e4f2879aea42bbd049ed57e12b65c878e101af4
VL1507 <vova.mashyanov@gmail.com>
2026-01-23 21:17:36 +1000

    .

--- data-generator/src/__main__.py
+++ data-generator/src/__main__.py
@@ -1,13 +1,26 @@
 import logging
 
+from bass.bass_api import Back4AppApi
+from bass.gen_insert_bass import populate_bass
+from config import BACK4APP_APPLICATION_ID, BACK4APP_REST_API_KEY
 from db.db_conn import Session
 from db.gen_insert_db import populate_database
 
 
 def main() -> None:
-    logging.basicConfig(level=logging.DEBUG)
-    with Session() as session:
-        populate_database(session, clear_first=True)
+    logging.basicConfig(level=logging.INFO)
+
+    # Загрузка в бд
+
+    # with Session() as session:
+    #     populate_database(session, clear_first=True)
+
+    #  загрузка в back4app
+    back4app_api = Back4AppApi(
+        application_id=BACK4APP_APPLICATION_ID,
+        rest_api_key=BACK4APP_REST_API_KEY,
+    )
+    populate_bass(back4app_api)
 
 
 if __name__ == "__main__":
